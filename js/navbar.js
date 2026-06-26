1c5736e5beb48a6a2cd93e06bc8dd34cfdbb153b
dkalabukhov <daniil.kalabukhov@gmail.com>
2026-06-26 20:04:51 +0300

    Add Navbar Background On Scroll

--- js/navbar.js
+++ js/navbar.js
@@ -5,10 +5,21 @@ export class Navbar {
       '.navbar__mobile-menu-toggle',
     );
     this.mobileMenuItems = this.el.querySelector('.navbar__mobile-menu-items');
-    this.mobileMenuToggleButton.addEventListener('click', () => this.toggle());
+    this.mobileMenuToggleButton.addEventListener('click', () =>
+      this.toggleMobileMenu(),
+    );
+    window.addEventListener('scroll', () => this.addBackgroundOnScroll());
   }
 
-  toggle() {
+  toggleMobileMenu() {
     this.mobileMenuItems.classList.toggle('navbar__mobile-menu-items_active');
+  }
+
+  addBackgroundOnScroll() {
+    if (window.scrollY > 0) {
+      this.el.classList.add('navbar_scroll');
+    } else {
+      this.el.classList.remove('navbar_scroll');
+    }
   }
 }
